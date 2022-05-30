1d0a1e5089c88da6c9feb4e2e691c6d2a65f9bec
Matthias Druwé <matthias.druwe@gmail.com>
2022-05-30 16:13:02 +0200

    Linq examples

---
+++ LinqExample/LinqExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 40, 24, 43, 246, 54, 34 };
+            numbers.Add(40);
+            numbers.Add(24);
+            numbers.Add(43);
+            numbers.Add(346);
+            numbers.Add(54);
+            numbers.Add(34);
+
+
+
+            Console.WriteLine(numbers.Average());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LinqExample/LinqUserExample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqUserExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>()
+            {
+                new User("Matthias", 32),
+                new User("Jef", 35),
+                new User("Marie", 29),
+                new User("Kim", 34)
+            };
+
+            double averageAge = users.Average(GetAge);
+            double averageAge1 = users.Average((user) =>
+            {
+                return user.Age;
+            });
+
+
+            List<string> oldestUsers = users
+                .Where((user) => user.Age > 32)
+                .OrderByDescending((user)=> user.Age)
+                .Select((user) => user.Name)
+                .ToList();
+
+            Console.WriteLine(averageAge1);
+            Console.WriteLine(oldestUsers);
+
+            Console.ReadKey();
+        }
+
+
+        private static int GetAge(User u)
+        {
+            return u.Age;
+        }
+
+
+
+    }
+
+    class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
